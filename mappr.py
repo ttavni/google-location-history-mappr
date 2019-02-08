6061d4e1d01bfc73e728569621f834f42048b5d3
Timothy Avni <timothy.avni@wunderman.com>
2019-02-08 14:11:44 +0000

    streamline mappr

--- mappr.py
+++ mappr.py
@@ -16,21 +16,23 @@ def json_to_df(filepath):
 
 	return location_data
 
-def line(df, filename='line.html',colour='blue',edge_width=1):
+def minmax(df):
 
 	min_lat, max_lat = min(df['latitudeE7']), max(df['latitudeE7'])
 	min_lon, max_lon = min(df['longitudeE7']), max(df['longitudeE7'])
 
-	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
+	return min_lat, max_lat, min_lon, max_lon
 
+def line(df, filename='line.html',colour='blue',edge_width=1):
+
+	min_lat, max_lat, min_lon, max_lon = minmax(df)
+	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
 	mymap.plot(df.latitudeE7, df.longitudeE7, colour, edge_width=edge_width)
 	mymap.draw(filename)
 
 def scatter(df, filename='scatter.html',colour='red',size=15,marker=False):
 
-	min_lat, max_lat = min(df['latitudeE7']), max(df['latitudeE7'])
-	min_lon, max_lon = min(df['longitudeE7']), max(df['longitudeE7'])
-
+	min_lat, max_lat, min_lon, max_lon = minmax(df)
 	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
 	mymap.scatter(tuple(df.latitudeE7), tuple(df.longitudeE7), colour, size=size, marker=marker)
 	mymap.draw(filename)
@@ -38,10 +40,7 @@ def scatter(df, filename='scatter.html',colour='red',size=15,marker=False):
 
 def heatmap(df, filename='heatmap.html',threshold=1,radius=500):
 
-	min_lat, max_lat = min(df['latitudeE7']), max(df['latitudeE7'])
-	min_lon, max_lon = min(df['longitudeE7']), max(df['longitudeE7'])
-
+	min_lat, max_lat, min_lon, max_lon = minmax(df)
 	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
-
 	mymap.heatmap(tuple(df.latitudeE7), tuple(df.longitudeE7), threshold=threshold, radius=radius)
 	mymap.draw(filename)
