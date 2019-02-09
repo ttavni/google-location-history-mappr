540b5d3b1bc7fbbe52c7c2d6b9ef04f62c3958b2
Tim Avni <33322296+tavni96@users.noreply.github.com>
2019-02-09 21:45:31 +0000

    efficiency

--- mappr.py
+++ mappr.py
@@ -20,27 +20,25 @@ def minmax(df):
 
 	min_lat, max_lat = min(df['latitudeE7']), max(df['latitudeE7'])
 	min_lon, max_lon = min(df['longitudeE7']), max(df['longitudeE7'])
+	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
 
-	return min_lat, max_lat, min_lon, max_lon
+	return mymap
 
 def line(df, filename='line.html',colour='blue',edge_width=1):
 
-	min_lat, max_lat, min_lon, max_lon = minmax(df)
-	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
+	mymap = minmax(df)
 	mymap.plot(df.latitudeE7, df.longitudeE7, colour, edge_width=edge_width)
 	mymap.draw(filename)
 
 def scatter(df, filename='scatter.html',colour='red',size=15,marker=False):
 
-	min_lat, max_lat, min_lon, max_lon = minmax(df)
-	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
+	mymap = minmax(df)
 	mymap.scatter(tuple(df.latitudeE7), tuple(df.longitudeE7), colour, size=size, marker=marker)
 	mymap.draw(filename)
 
 
 def heatmap(df, filename='heatmap.html',threshold=1,radius=500):
 
-	min_lat, max_lat, min_lon, max_lon = minmax(df)
-	mymap = gmplot.GoogleMapPlotter((max_lat + min_lat) / 2, (max_lon + min_lon) / 2, 7,apikey=api_key)
+	mymap = minmax(df)
 	mymap.heatmap(tuple(df.latitudeE7), tuple(df.longitudeE7), threshold=threshold, radius=radius)
 	mymap.draw(filename)
